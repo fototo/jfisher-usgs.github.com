622a9e5e5ba9a714d025d7b501043fcb5fdb4ed4
Jason Fisher <jfisher@usgs.gov>
2012-07-02 13:55:46 -0600

    Center figures

--- R/MakePost.R
+++ R/MakePost.R
@@ -4,6 +4,7 @@ KnitGitHub <- function(input, base.url="/") {
   
   fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
   opts_chunk$set(fig.path=fig.path)
+  opts_chunk$set(fig.cap="center")
   
   render_jekyll()
   knit(input, envir=parent.frame())
@@ -19,6 +20,6 @@ setwd("E:/WORK/JFisher/Software/RPosts")
 ###
 
 path <- "E:/WORK/JFisher/Software/jfisher-usgs.github.com"
-input <- file.path(path, "Rmd", "2012-07-02-knitr-markdown-jekyll.Rmd")
+input <- file.path(path, "Rmd", "2012-07-02-knitr-jekyll.Rmd")
 
 KnitGitHub(input)
