3c9849f529d27712cb174ae288e65c47c1428f14
Jason Fisher <jfisher@usgs.gov>
2012-07-02 13:36:18 -0600

    Added R and Rmd source

---
+++ R/MakePost.R
@@ -0,0 +1,24 @@
+
+KnitGitHub <- function(input, base.url="/") {
+  opts_knit$set(base.url=base.url)
+  
+  fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
+  opts_chunk$set(fig.path=fig.path)
+  
+  render_jekyll()
+  knit(input, envir=parent.frame())
+}
+
+###
+
+library(markdown)
+library(knitr)
+
+setwd("E:/WORK/JFisher/Software/RPosts")
+
+###
+
+path <- "E:/WORK/JFisher/Software/jfisher-usgs.github.com"
+input <- file.path(path, "Rmd", "2012-07-02-knitr-markdown-jekyll.Rmd")
+
+KnitGitHub(input)
